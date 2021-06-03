d847224318a909e219743f0fb7ed09885ce7e760
guastallaigor <limaguastallaigor@gmail.com>
2021-06-03 18:51:43 -0300

    docs: fix example

--- docs/components/Example.vue
+++ docs/components/Example.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="testimonials">
+  <div>
     <img src="@/static/1.png" alt="First image" />
     <img class="middle-image" src="@/static/2.png" alt="Second image" />
     <img class="last-image" src="@/static/3.png" alt="Third image" />
@@ -7,35 +7,6 @@
 </template>
 
 <style>
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
-body,
-html {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  width: 100%;
-  height: auto;
-  background-color: #111827;
-}
-#app {
-  width: 100%;
-  height: 100%;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-    sans-serif;
-  -webkit-font-smoothing: antialiased;
-}
-#main-title {
-  color: #ffffff;
-  font-size: 3rem;
-  margin-bottom: 70px;
-  text-align: center;
-}
 .middle-image {
   margin: 50px 0;
 }
