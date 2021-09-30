5f36ae05f142f94d0eee582772ab75cd54f16204
Igor Guastalla de Lima <ilima@denovo-us.com>
2021-09-29 21:31:11 -0300

    chore: add-storybook. add settings vs-code

---
+++ .storybook/main.ts
@@ -0,0 +1,25 @@
+const svgLoader = require('vite-svg-loader')
+const path = require('path')
+
+module.exports = {
+  stories: [
+    '../stories/**/*.stories.mdx',
+    '../stories/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  core: {
+    builder: 'storybook-builder-vite',
+  },
+  viteFinal: (config) => {
+    config.plugins = [...config.plugins, svgLoader()]
+    if (process.env.NODE_ENV === 'production') {
+      config.build.chunkSizeWarningLimit = 1200
+    }
+    config.resolve.modules = [path.resolve(__dirname, '..'), 'node_modules']
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, '../lib'),
+    }
+    return config
+  },
+}
---
+++ .storybook/preview.ts
@@ -0,0 +1,12 @@
+import { Parameters } from '@storybook/addons'
+import './storybook.css'
+
+export const parameters: Parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
