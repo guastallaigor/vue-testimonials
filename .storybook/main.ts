18d173fed4799a8f700c8bf33dccfe2c03c841af
Igor Guastalla de Lima <ilima@denovo-us.com>
2021-09-30 23:40:10 -0300

    fix: fix storybook config bug

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -20,9 +20,6 @@ module.exports = {
       ...config.resolve.alias,
       '@': path.resolve(__dirname, '../lib'),
     }
-    if (config.resolve.extensions && Array.isArray(config.resolve.extensions))
-      config.resolve.extensions.push('.ts', '.tsx')
-    else config.resolve.extensions = ['.ts', '.tsx']
     return config
   },
 }
